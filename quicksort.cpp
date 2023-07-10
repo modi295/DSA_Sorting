84507a190178b9074650b70490d686d0309632dc
Chaman Modi <59691163+modi295@users.noreply.github.com>
2023-07-10 14:05:11 +0530

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high]; 
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+
+
+
+int main(){
+    int n;
+    cout<<"enter the size of array";
+    cin>>n;
+    int a[n];
+    cout<<"enter element in array";
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    quickSort(a,0,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i];
+    }
+}
